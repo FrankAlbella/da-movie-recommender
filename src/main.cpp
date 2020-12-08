74569588a7bbae306bda33a5f29113be758f389d
FrankAlbella <FrankAlbella@gmail.com>
2020-12-07 20:23:02 -0500

    Add timekeeping and input validation

--- src/main.cpp
+++ src/main.cpp
@@ -354,65 +354,48 @@ bool LoadMasterFile(std::map<std::string, Movie *> &moviesByName, std::map<std::
     return true;
 }
 
-std::vector <Movie*> selectionSort(int loops, std::map<std::string, Movie*> &moviesByName) //Cut down to only correct number of values
+std::vector<Movie *> selectionSort(int loops, std::map<std::string, Movie *> &moviesByName) //Cut down to only correct number of values
 {
     int score = 0;
-    int count = loops;
+    loops += 1;
 
-    std::vector<Movie*> sorted;
+    std::vector<Movie *> sorted;
 
-    if (count > moviesByName.size() or count < 0)
-    {
-        count = moviesByName.size();
-    }
-	else
-	{
-		count = count + 1;
-	}
-
+    if (loops > moviesByName.size() or loops < 0)
+        loops = moviesByName.size();
 
     for (auto it = moviesByName.begin(); it != moviesByName.end(); it++)
-    {
         sorted.push_back(it->second);
-    }
 
     int index = 0;
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < loops; i++)
     {
         for (int x = i; x < sorted.size(); x++)
-        {
             if (sorted[index]->score < sorted[x]->score)
-            {
                 index = x;
-            }
-        }
 
         Movie *temp = sorted[i];
         sorted[i] = sorted[index];
         sorted[index] = temp;
     }
-    return sorted; 
-}   
+    return sorted;
+}
 
-void heapifyDown(std::vector<Movie*> &theHeap, int index, int size)
+void heapifyDown(std::vector<Movie *> &theHeap, int index, int size)
 {
     int leftChild = 2 * index + 1;
     int rightChild = 2 * index + 2;
     int biggest = index;
 
     if (leftChild < size && theHeap[leftChild]->score > theHeap[biggest]->score)
-    {
         biggest = leftChild;
-    }
 
     if (rightChild < size && theHeap[rightChild]->score > theHeap[biggest]->score)
-    {
         biggest = rightChild;
-    }
 
     if (biggest != index)
     {
-        Movie* temp = theHeap[index];
+        Movie *temp = theHeap[index];
         theHeap[index] = theHeap[biggest];
         theHeap[biggest] = temp;
 
@@ -420,21 +403,16 @@ void heapifyDown(std::vector<Movie*> &theHeap, int index, int size)
     }
 }
 
-std::vector <Movie*> heapSort(int loops, std::map<std::string, Movie*> &moviesByName)
+std::vector<Movie *> heapSort(int loops, std::map<std::string, Movie *> &moviesByName)
 {
-    std::vector <Movie*> sorted;
-    std::vector <Movie*> output;
-
+    std::vector<Movie *> sorted;
+    std::vector<Movie *> output;
 
     for (auto it = moviesByName.begin(); it != moviesByName.end(); it++)
-    {
         sorted.push_back(it->second);
-    }
 
     for (int i = sorted.size() / 2 - 1; 0 <= i; i--)
-    {
         heapifyDown(sorted, i, sorted.size());
-    }
 
     for (int i = 0; i < loops; i++)
     {
@@ -447,242 +425,240 @@ std::vector <Movie*> heapSort(int loops, std::map<std::string, Movie*> &moviesBy
     return output;
 }
 
-std::vector< std::pair<int, Movie*>> suggestAlt(std::string title, std::map<std::string, Movie*>& movies)
+std::vector<Movie *> suggestAlt(std::string title, std::map<std::string, Movie *> &movies)
 {
-    std::vector< std::pair<int, Movie*>> suggestions;
+    std::vector<Movie *> suggestions;
 
     for (auto it = movies.begin(); it != movies.end(); it++)
-    {
         if (it->second->name.find(title) != std::string::npos)
-        {
-            suggestions.push_back(std::make_pair(it->second->ratings, it->second));
-        }
-    }
+            suggestions.push_back(it->second);
+
     return suggestions;
 }
 
-Movie* findMovie(std::string title, std::string year, std::map<std::string, Movie*> &movies) //Todo account for a null search result
+Movie *findMovie(std::string title, std::string year, std::map<std::string, Movie *> &movies) //Todo account for a null search result
 {
     std::string key = title + "_" + year;
+    Movie *output = nullptr;
 
     auto it = movies.find(key);
-
-    Movie* output = NULL;
-
     if (it != movies.end())
+        return it->second;
+
+    std::cout << "Movie: " << title << " not found!" << std::endl
+              << std::endl;
+    std::vector<Movie *> suggestions = suggestAlt(title, movies);
+    sort(suggestions.begin(), suggestions.end(), [](Movie *a, Movie *b) { return a->ratings > b->ratings; });
+
+    if (suggestions.size() < 1)
+        return nullptr;
+
+    std::cout << "Did you mean?" << std::endl;
+
+    int loops = 10;
+    if (suggestions.size() < 10)
+        loops = suggestions.size();
+
+    for (int i = 0; i < loops; i++)
+        std::cout << i + 1 << ". "
+                  << "\"" << suggestions[i]->name << "\", " << suggestions[i]->year << std::endl;
+
+    int choice = -1;
+    
+    while (choice < 0 || choice > suggestions.size())
     {
-        output = it->second;
+        std::cout << "Pick an option: ";
+        std::cin >> choice;
+        std::cin.get();
+
+        if(choice < 0 || choice > suggestions.size())
+            std::cout << "Invalid choice, please try again." << std::endl;
     }
 
-    else
-    {
-        std::cout << "Movie: " << title << " not found!" << std::endl << std::endl;
-        std::vector< std::pair<int, Movie*>> suggestions = suggestAlt(title, movies);
-        sort(suggestions.begin(), suggestions.end(), std::greater <std::pair<int, Movie*>>());
-
-        if (suggestions.size() > 0)
-        {
-            std::cout << "Did you mean?" << std::endl;
-
-            int loops = 0;
-            if (suggestions.size() < 10)
-            {
-                loops = suggestions.size();
-            }
-            
-            else
-            {
-                loops = 10;
-            }
-            for (int i = 0; i < loops; i++)
-            {
-                std::cout << i + 1 << ". " << "\"" << suggestions[i].second->name << "\", "<< suggestions[i].second->year << std::endl;
-            }
-
-            int x;
-            std::cout << "Pick an option: ";
-            std::cin >> x;
-            output = suggestions[x - 1].second;
-        }
-
-        else
-        {
-            std::cout << "No results" << std::endl;
-        }
-    }
-
-    return output; 
+    return suggestions[choice - 1];
 }
 
-void scoreMovies(std::map<std::string, Movie*> movies, Movie* selectedMovie)
+void scoreMovies(std::map<std::string, Movie *> movies, Movie *selectedMovie)
 {
-	float k_rs = 1;
-	float k_ra = 1;
-	float k_y = -0.25;
-	float k_g = 1.5;
-	float k_a = 1.5;
-	float k_w = 1;
-	float k_d = 1.6;
+    float k_rs = 1;
+    float k_ra = 1;
+    float k_y = -0.25;
+    float k_g = 1.5;
+    float k_a = 1.5;
+    float k_w = 1;
+    float k_d = 1.6;
 
+    if (selectedMovie->year == "UNKNOWN")
+        selectedMovie->year = "1900";
 
-	//goes through map of movies, and scores them based on input movie
-	for (auto it = movies.begin(); it != movies.end(); it++)
-	{
-		Movie* thisMovie = it->second;
+    //goes through map of movies, and scores them based on input movie
+    for (auto it = movies.begin(); it != movies.end(); it++)
+    {
+        Movie *thisMovie = it->second;
 
-		float ratingsScore = 1 - abs((float(thisMovie->ratings) - float(selectedMovie->ratings))) / (float(thisMovie->ratings) + float(selectedMovie->ratings) + float(1));
-		float ratingScore = (thisMovie->avgRating);
-		float yearScore = abs(stof(selectedMovie->year) - stof(thisMovie->year)) + 1;
-		float genreScore = 0;
-		float actorScore = 1;
-		float writerScore = 1;
-		float directorScore = 1;
+        if (thisMovie->year == "UNKNOWN")
+            thisMovie->year = "1900";
 
-		for (auto it = thisMovie->genres.begin(); it != thisMovie->genres.end(); it++) {
-			genreScore += selectedMovie->genres.count(*it);
-		}
-		for (auto it = thisMovie->actorIds.begin(); it != thisMovie->actorIds.end(); it++) {
-			actorScore += selectedMovie->actorIds.count(*it);
-		}
-		for (auto it = thisMovie->writerIds.begin(); it != thisMovie->writerIds.end(); it++) {
-			writerScore += selectedMovie->writerIds.count(*it);
-		}
-		for (auto it = thisMovie->directorIds.begin(); it != thisMovie->directorIds.end(); it++) {
-			directorScore += selectedMovie->directorIds.count(*it);
-		}
+        float ratingsScore = 1 - abs((float(thisMovie->ratings) - float(selectedMovie->ratings))) / (float(thisMovie->ratings) + float(selectedMovie->ratings) + float(1));
+        float ratingScore = (thisMovie->avgRating);
+        float yearScore = abs(stof(selectedMovie->year) - stof(thisMovie->year)) + 1;
+        float genreScore = 0;
+        float actorScore = 1;
+        float writerScore = 1;
+        float directorScore = 1;
 
-		float thisRecomendationScore = pow(ratingsScore, k_rs) * pow(ratingScore, k_ra) * pow(yearScore, k_y) * pow(genreScore, k_g) * pow(actorScore, k_a) * pow(writerScore, k_w) * pow(directorScore, k_d);
+        for (auto genre : thisMovie->genres)
+            genreScore += selectedMovie->genres.count(genre);
 
-		thisMovie->score = thisRecomendationScore;
-	}
+        for (auto actorId : thisMovie->actorIds)
+            actorScore += selectedMovie->actorIds.count(actorId);
+
+        for (auto writerId : thisMovie->writerIds)
+            writerScore += selectedMovie->writerIds.count(writerId);
+
+        for (auto directorId : thisMovie->directorIds)
+            directorScore += selectedMovie->directorIds.count(directorId);
+
+        float thisRecomendationScore = pow(ratingsScore, k_rs) * pow(ratingScore, k_ra) * pow(yearScore, k_y) * pow(genreScore, k_g) * pow(actorScore, k_a) * pow(writerScore, k_w) * pow(directorScore, k_d);
+
+        thisMovie->score = thisRecomendationScore;
+    }
 }
 
 int main()
 {
-	// Key: NAME_YEAR
-	std::map<std::string, Movie*> moviesByName;
+    // Key: NAME_YEAR
+    std::map<std::string, Movie *> moviesByName;
 
-	// Stores a ID:NAME pair, so we can have fast lookups by both names and ID
-	std::map<std::string, Movie*> moviesById;
+    // Stores a ID:NAME pair, so we can have fast lookups by both names and ID
+    std::map<std::string, Movie *> moviesById;
 
-	bool masterFileUsed = false;
+    bool masterFileUsed = false;
 
-	auto startTime = std::chrono::high_resolution_clock::now();
+    auto startTime = std::chrono::high_resolution_clock::now();
 
-	if (masterFileUsed = LoadMasterFile(moviesByName, moviesById))
-		goto finished_loading;
+    if (masterFileUsed = LoadMasterFile(moviesByName, moviesById))
+        goto finished_loading;
 
-	std::cout << "Loading database..." << std::endl;
-	if (!LoadMovieBasics(moviesByName, moviesById))
-	{
-		std::cout << "Missing file: " << TITLE_BASICS << std::endl;
-		return 1;
-	}
+    std::cout << "Loading database..." << std::endl;
+    if (!LoadMovieBasics(moviesByName, moviesById))
+    {
+        std::cout << "Missing file: " << TITLE_BASICS << std::endl;
+        return 1;
+    }
 
-	std::cout << "Loading ratings..." << std::endl;
-	if (!LoadRatings(moviesById))
-	{
-		std::cout << "Missing file: " << TITLE_RATINGS << std::endl;
-		return 1;
-	}
+    std::cout << "Loading ratings..." << std::endl;
+    if (!LoadRatings(moviesById))
+    {
+        std::cout << "Missing file: " << TITLE_RATINGS << std::endl;
+        return 1;
+    }
 
-	std::cout << "Loading cast..." << std::endl;
-	if (!LoadCastIds(moviesById))
-	{
-		std::cout << "Missing file: " << TITLE_PRINCIPALS << std::endl;
-		return 1;
-	}
+    std::cout << "Loading cast..." << std::endl;
+    if (!LoadCastIds(moviesById))
+    {
+        std::cout << "Missing file: " << TITLE_PRINCIPALS << std::endl;
+        return 1;
+    }
 
-	std::cout << "Processing names..." << std::endl;
-	if (!LoadCastNames(moviesById))
-	{
-		std::cout << "Missing file: " << NAME_BASICS << std::endl;
-		return 1;
-	}
+    std::cout << "Processing names..." << std::endl;
+    if (!LoadCastNames(moviesById))
+    {
+        std::cout << "Missing file: " << NAME_BASICS << std::endl;
+        return 1;
+    }
 
-	std::cout << "Loading languages..." << std::endl;
-	if (!LoadLanguages(moviesById))
-	{
-		std::cout << "Missing file: " << TITLE_AKAS << std::endl;
-		return 1;
-	}
+    std::cout << "Loading languages..." << std::endl;
+    if (!LoadLanguages(moviesById))
+    {
+        std::cout << "Missing file: " << TITLE_AKAS << std::endl;
+        return 1;
+    }
 
 finished_loading:
-	auto stopTime = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stopTime - startTime);
-	std::cout << "Time elasped: " << duration.count() << "ms" << std::endl;
+    auto stopTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stopTime - startTime);
+    std::cout << "Time elasped: " << duration.count() << "ms" << std::endl;
 
-	if (!masterFileUsed)
-	{
-		std::cout << "A master file can be created for faster loading times" << std::endl;
-		std::cout << "Would you like to create a master file? (y/n): ";
+    if (!masterFileUsed)
+    {
+        std::cout << "A master file can be created for faster loading times" << std::endl;
+        std::cout << "Would you like to create a master file? (y/n): ";
 
-		std::string answer;
+        std::string answer;
 
-		std::cin >> answer;
+        std::cin >> answer;
+        std::cin.get();
 
-		if ((unsigned char)std::tolower(answer[0]) == 'y')
-		{
-			std::cout << "Saving to master file..." << std::endl;
-			MoviesToTsv(moviesById);
-		}
+        if ((unsigned char)std::tolower(answer[0]) == 'y')
+        {
+            std::cout << "Saving to master file..." << std::endl;
+            MoviesToTsv(moviesById);
+        }
+    }
 
-	}
+    while (true)
+    {
+        std::string title;
+        std::string year;
 
-	while (true)
-	{
-		std::string title;
-		std::string year;
+        std::cout << "Enter a Movie Name: ";
+        getline(std::cin, title);
+        std::cout << "Enter the year published (type UNKNOWN if the movie has no known date): ";
+        getline(std::cin, year);
 
-		std::cout << "Enter a Movie Name: ";
-		getline(std::cin, title);
-		std::cout << "Enter the year published (type UNKNOWN if the movie has no known date): ";
-		getline(std::cin, year);
+        Movie *selectedMovie = findMovie(title, year, moviesByName);
 
-		//Not needed with after search suggestion function
-		//std::string key = title + "_" + year; 
-		//if (moviesByName.count(key) == 0)
-		//{
-		//	std::cout << "Movie not found!" << std::endl;
-		//	return 2;
-		//}
+        if (selectedMovie == nullptr)
+            continue;
 
-		Movie* selectedMovie = findMovie(title, year, moviesByName);
+        selectedMovie->print();
 
-		selectedMovie->print();
+        scoreMovies(moviesByName, selectedMovie);
 
-		std::string imdbId = selectedMovie->movieId;
+        std::cout << "Top 10 Recommendations by selection sort" << std::endl;
 
-		scoreMovies(moviesByName, selectedMovie);
+        startTime = std::chrono::high_resolution_clock::now();
 
-		std::cout << "Top 10 Recommendations; selection sort" << std::endl;
-		auto bsorted = selectionSort(10, moviesByName);
-		int bshift = 0;
-		if (bsorted[0]->name == selectedMovie->name) {
-			bshift = 1;
-		}
-		for (int i = 0 + bshift; i < 10 + bshift; i++)
-		{
-			std::cout << bsorted[i]->name << " " << bsorted[i]->score << std::endl;
-		}
+        auto bsorted = selectionSort(10, moviesByName);
 
-		std::cout << std::endl;
+        stopTime = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::milliseconds>(stopTime - startTime);
+        std::cout << "Time elasped: " << duration.count() << "ms" << std::endl;
 
-		std::cout << "Top 10 Recommendations; heapsort sort" << std::endl;
-		auto hsorted = heapSort(10+1, moviesByName);
-		int hshift = 0;
-		if (hsorted[0]->name == selectedMovie->name) {
-			hshift = 1;
-		}
-		for (int i = 0 + hshift; i < 10 + hshift; i++)
-		{
-			std::cout << hsorted[i]->name << " " << hsorted[i]->score << std::endl;
-		}
-		title = "";
-		year = "";
+        int bshift = bsorted[0]->movieId == selectedMovie->movieId;
 
-		std::cout << std::endl;
-	}
+        for (int i = 0 + bshift; i < 10 + bshift; i++)
+            std::cout << bsorted[i]->name << " " << bsorted[i]->score << std::endl;
 
-	return 0;
+        std::cout << std::endl;
+
+        std::cout << "Top 10 Recommendations by heapsort sort" << std::endl;
+
+        startTime = std::chrono::high_resolution_clock::now();
+
+        auto hsorted = heapSort(10 + 1, moviesByName);
+
+        stopTime = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::milliseconds>(stopTime - startTime);
+        std::cout << "Time elasped: " << duration.count() << "ms" << std::endl;
+
+        int hshift = hsorted[0]->movieId == selectedMovie->movieId;
+
+        for (int i = 0 + hshift; i < 10 + hshift; i++)
+            std::cout << hsorted[i]->name << " " << hsorted[i]->score << std::endl;
+
+        std::cout << "Would you like search another movie? (y/n): ";
+
+        std::string answer;
+        std::cin >> answer;
+        std::cin.get();
+
+        if ((unsigned char)std::tolower(answer[0]) == 'n')
+            break;
+
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
